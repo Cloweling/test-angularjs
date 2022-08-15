42eecfb994279639e0b1a2a85689cc828c2ac133
Renato Ayau <renato.ayau@gmail.com>
2022-08-14 20:30:46 -0600

    init

--- src/js/index.js
+++ src/js/index.js
@@ -25,28 +25,6 @@ angular
 
       $scope.submit = function () {
         // do something
-        if ($scope.input1 === null) {
-          return false;
-        }
-
-        if ($scope.input2 === null) {
-          return false;
-        }
-
-        if ($scope.input3 === null) {
-          return false;
-        }
-
-        if ($scope.input1 % 2 !== 0) {
-          $scope.result1 = "Yes";
-        } 
-
-        if ($scope.input2 > 18) {
-           $scope.result2 = "Yes";
-        }
-
-        $scope.result3 = $scope.input3.length;
-
       };
     },
   ]);
